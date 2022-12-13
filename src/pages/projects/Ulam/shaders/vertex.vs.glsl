d41e3651fab9457bc575b0c8ad15d36d3cdf004a
Danilo Arantes <dfilho@purdue.edu>
2022-12-13 15:49:14 -0300

    ulam using attributes

--- src/pages/projects/Ulam/shaders/vertex.vs.glsl
+++ src/pages/projects/Ulam/shaders/vertex.vs.glsl
@@ -1,12 +1,16 @@
 varying vec2 vUv;
 varying vec3 vNormal;
 uniform float uTime;
+attribute float isPrime;
+varying float vNumber;
 
 void main() {
   vUv = uv;
   vNormal = normalize(normalMatrix * normal);
 
+  vNumber = isPrime;
+
   // Update vertex position
-  gl_PointSize = 55.;
+  gl_PointSize = 3.;
   gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 }
