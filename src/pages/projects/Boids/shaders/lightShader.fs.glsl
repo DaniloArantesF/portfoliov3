dbc0e5f05ed59c9227270822df48fd0928b04d52
Danilo Arantes <dfilho@purdue.edu>
2022-12-23 17:07:46 -0300

    instanced mesh setup working

--- src/pages/projects/Boids/shaders/lightShader.fs.glsl
+++ src/pages/projects/Boids/shaders/lightShader.fs.glsl
@@ -6,13 +6,16 @@ precision mediump float;
 
 uniform vec2 uResolution;
 uniform float uTime;
+uniform float index;
+uniform vec3 randomColor;
+
 varying vec2 vUv;
 varying vec3 vNormal;
 varying vec3 vPosition;
 
 void main() {
-  vec3 color = vec3(0., 1., 0.);
-  color *= step(.5, abs(sin(uTime * 2.)));
+  vec3 color = randomColor;
+  color *= step(.5, abs(sin(index*200. + uTime)));
 
   gl_FragColor = vec4(color,1.);
 }
