ee8c6958c2f1d0e9ccbb67f2fdd4fd49dd700334
Danilo Arantes <dfilho@purdue.edu>
2023-01-07 17:23:08 -0300

    new scene init + glsl plugin setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/static';
+import glsl from 'vite-plugin-glsl';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: 'static',
   adapter: vercel(),
+  vite: {
+    plugins: [
+      glsl({
+        include: ['**/*.vs.glsl', '**/*.fs.glsl'],
+        watch: false,
+        // compress: true,
+      }),
+    ],
+  },
 });
