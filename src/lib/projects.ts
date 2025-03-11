d37148764d65757441ee038d5c1ac97d6b9138bb
Danilo Arantes F. <dfilho@purdue.edu>
2025-03-11 19:07:12 -0300

    feat: Revert to use local projects from markdown files

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,40 +1,53 @@
-// import CardList from '../components/CardList';
-// import Layout from '../layouts/Layout.astro';
-// import type { Tag } from '../payload-types';
+import type { MarkdownInstance } from 'astro';
 
-// export interface ProjectProps {
-//   description: string;
-//   featured: boolean;
-//   github?: string;
-//   lastUpdated: string;
-//   live?: string;
-//   image: string;
-//   sandbox?: string;
-//   subtitle: string;
-//   href?: string;
-//   slug?: string;
-//   tags: Tag[];
-//   title: string;
-//   visibility?: 'visible' | 'hidden' | 'private';
-//   type?: 'project' | 'scene' | 'game';
-// }
+export interface ProjectProps {
+  description: string;
+  featured: boolean;
+  github?: string;
+  lastUpdated: string;
+  live?: string;
+  image: string;
+  sandbox?: string;
+  subtitle: string;
+  href?: string;
+  slug?: string;
+  // tags: Tag[];
+  title: string;
+  visibility?: 'visible' | 'hidden' | 'private';
+  type?: 'project' | 'scene' | 'game';
+}
 
-// export async function getLocalProjects(all = false) {
-//   // Hidden projects are only visible in production.
-//   // Show all if dev
-//   const visible = (status: string) =>
-//     (all || import.meta.env.DEV || status === 'visible') && status !== 'hidden';
+export async function getLocalProjects(all = false) {
+  // Hidden projects are only visible in production.
+  // Show all if dev
+  const visible = (status: string) =>
+    (all || import.meta.env.DEV || status === 'visible') && status !== 'hidden';
 
-//   // Offsite projects are included from src/projects
-//   const projects = (
-//     await Astro.glob<ProjectProps>('/src/projects/*.md')
-//   ).filter(({ frontmatter }) => visible(frontmatter.visibility ?? ''));
+  // Offsite projects are included from src/projects
+  const projectsFiles = import.meta.glob<MarkdownInstance<ProjectProps>>(
+    '/src/projects/*.md',
+  );
 
-//   // Demos are included here
-//   // Note: If a demo does not contain a readme it will not be listed
-//   const demos = (
-//     await import.meta.glob<ProjectProps>('/src/pages/projects/**/*.md')
-//   ).filter(({ frontmatter }) => visible(frontmatter.visibility ?? ''));
+  let projects = await Promise.all(
+    Object.keys(projectsFiles).map(async (file) => await projectsFiles[file]()),
+  );
 
-//   return [...projects, ...demos];
-// }
+  projects = projects.filter(({ frontmatter }) =>
+    visible(frontmatter.visibility ?? ''),
+  );
+
+  // Demos are included here
+  // Note: If a demo does not contain a readme it will not be listed
+  const demoFiles = import.meta.glob<MarkdownInstance<ProjectProps>>(
+    '/src/pages/projects/**/*.md',
+  );
+  let demos = await Promise.all(
+    Object.keys(demoFiles).map(async (file) => await demoFiles[file]()),
+  );
+
+  demos = demos.filter(({ frontmatter }) =>
+    visible(frontmatter.visibility ?? ''),
+  );
+
+  return [...projects, ...demos];
+}
